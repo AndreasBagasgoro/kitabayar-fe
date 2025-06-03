0e86dbd0c1aec8de4ea61a897dd4afe88c2446ec
AndreasBagasgoro <andreasbagasgoro04@gmail.com>
2025-06-04 02:50:54 +0700

    nambahin modal pembayaran

--- src/app/components/dashboard/sidebar/page.tsx
+++ src/app/components/dashboard/sidebar/page.tsx
@@ -48,9 +48,15 @@ const Sidebar: React.FC = () => {
           className="flex items-center space-x-3 cursor-pointer"
           onClick={() => router.push('/')}
         >
-          <div className="w-10 h-10 bg-gradient-to-r from-pink-500 to-red-500 rounded-full flex items-center justify-center">
-            <div className="w-6 h-6 bg-white rounded-full opacity-80"></div>
-          </div>
+          <div className="w-8 h-8 bg-gradient-to-br from-[#d9291a] to-pink-400 rounded-lg flex items-center justify-center">
+                  <svg
+                    className="w-5 h-5 text-white"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
+                  </svg>
+                </div>
           <h1 className="text-xl font-bold text-gray-800">kITA BAYAR</h1>
         </div>
       </div>
